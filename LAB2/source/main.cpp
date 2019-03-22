43aacb80677d4532038e57b1e6f3c75f74d7a8a1
DominikSucharski <dominik1900@o2.pl>
2019-03-22 03:05:24 +0100

    test

--- LAB2/source/main.cpp
+++ LAB2/source/main.cpp
@@ -1,9 +1,26 @@
 #include <Windows.h>;
 #include "res.h"
 
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
+  return DefWindowProc(hwnd, message, wParam, lParam); //wywo³¹nie procedury okna
+}
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow) {
   HWND hwndMainWindow = CreateDialog(hInstance, MAKEINTRESOURCE(IDD_MAINVIEW), NULL, NULL);
   ShowWindow(hwndMainWindow, iCmdShow);
+  WNDCLASS wc;
+  wc.style = CS_HREDRAW | CS_VREDRAW;
+  wc.lpfnWndProc = (WNDPROC) WndProc;
+  wc.cbClsExtra = 0;
+  wc.cbWndExtra = 0;
+  wc.hInstance = hInstance;
+  wc.hIcon = 0;
+  wc.hCursor = 0;
+  wc.hbrBackground = 0;
+  wc.lpszMenuName = TEXT("Pierwsza klasa");
+  //rejestracja klasy okna
+  if (!RegisterClass(&wc)) return false;
+  MessageBox(0, TEXT("nazwa1"), TEXT("info"), MB_OK);
   while (1) {
 
   }
